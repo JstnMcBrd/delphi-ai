e3ce6c3af25add263bb55a78c2288cbc8e7f4554
Justin McBride <jstnmcbrd@outlook.com>
2023-09-25 01:57:53 -0600

    Replace jest with vitest

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,14 +1,15 @@
 import { basename } from "node:path";
 import { URL, URLSearchParams } from "node:url";
 
-import { afterAll, beforeEach, describe, expect, it, jest } from "@jest/globals";
-
-const mockFetch = jest.spyOn(global, "fetch");
+import { afterAll, describe, expect, it, vi } from "vitest";
 
 // Dynamically import the module to test
 const delphi = (await import("./index.js")).default;
 
 describe(basename(import.meta.url), () => {
+
+	const mockFetch = vi.fn<URL[], Promise<Response>>();
+	vi.stubGlobal("fetch", mockFetch);
 
 	const goodResponseText = "mock response";
 	const goodResponse = {
@@ -22,30 +23,27 @@ describe(basename(import.meta.url), () => {
 		statusText: "Not Found",
 	} as Response;
 
-	beforeEach(() => {
-		mockFetch.mockResolvedValue(goodResponse);
+	afterAll(() => {
+		vi.restoreAllMocks();
 	});
 
 	it("should replace empty input with whitespace", async () => {
+		mockFetch.mockResolvedValue(goodResponse);
 		await delphi("");
-		const url = mockFetch.mock.calls[0][0] as URL;
+		const url = mockFetch.mock.calls[0][0];
 		const params = new URLSearchParams(url.search);
 		const input = params.get("action1") ?? "";
 		expect(input).toEqual(" ");
 	});
 
-	it("should throw an error if it receives a bad response", async () => {
-		mockFetch.mockResolvedValue(badResponse);
-		await expect(delphi("test")).rejects.toThrowError();
-	});
-
-	it("it should return the answer text if it receives a good response", async () => {
+	it("should return the answer text if it receives a good response", async () => {
 		mockFetch.mockResolvedValue(goodResponse);
 		const output = await delphi("");
 		expect(output).toEqual(goodResponseText);
 	});
 
-	afterAll(() => {
-		jest.restoreAllMocks();
+	it("should throw an error if it receives a bad response", async () => {
+		mockFetch.mockResolvedValue(badResponse);
+		await expect(delphi("test")).rejects.toThrowError();
 	});
 });
