1696fef119c9ea3ba3779527bdb11e794305b035
Justin McBride <jstnmcbrd@outlook.com>
2024-01-12 22:22:00 -0700

    New ESLint configuration

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,16 +1,15 @@
-import { basename } from "node:path";
-import { URL, URLSearchParams } from "node:url";
+import { basename } from 'node:path';
+import { URL, URLSearchParams } from 'node:url';
 
-import { afterAll, describe, expect, it, vi } from "vitest";
+import { afterAll, describe, expect, it, vi } from 'vitest';
 
-import delphi from "./index.js";
+import delphi from './index.js';
 
 describe(basename(import.meta.url), () => {
-
 	const mockFetch = vi.fn<URL[], Promise<Response>>();
-	vi.stubGlobal("fetch", mockFetch);
+	vi.stubGlobal('fetch', mockFetch);
 
-	const goodResponseText = "mock response";
+	const goodResponseText = 'mock response';
 	const goodResponse = {
 		ok: true,
 		json: () => Promise.resolve({ answer: { text: goodResponseText } }),
@@ -19,30 +18,30 @@ describe(basename(import.meta.url), () => {
 	const badResponse = {
 		ok: false,
 		status: 404,
-		statusText: "Not Found",
+		statusText: 'Not Found',
 	} as Response;
 
 	afterAll(() => {
 		vi.restoreAllMocks();
 	});
 
-	it("should replace empty input with whitespace", async () => {
+	it('should replace empty input with whitespace', async () => {
 		mockFetch.mockResolvedValue(goodResponse);
-		await delphi("");
+		await delphi('');
 		const url = mockFetch.mock.calls[0][0];
 		const params = new URLSearchParams(url.search);
-		const input = params.get("action1") ?? "";
-		expect(input).toEqual(" ");
+		const input = params.get('action1') ?? '';
+		expect(input).toEqual(' ');
 	});
 
-	it("should return the answer text if it receives a good response", async () => {
+	it('should return the answer text if it receives a good response', async () => {
 		mockFetch.mockResolvedValue(goodResponse);
-		const output = await delphi("");
+		const output = await delphi('');
 		expect(output).toEqual(goodResponseText);
 	});
 
-	it("should throw an error if it receives a bad response", async () => {
+	it('should throw an error if it receives a bad response', async () => {
 		mockFetch.mockResolvedValue(badResponse);
-		await expect(delphi("test")).rejects.toThrowError();
+		await expect(delphi('test')).rejects.toThrowError();
 	});
 });
