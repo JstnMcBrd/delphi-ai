0f38a0bc228e854f33a4f9a68a1b9ef293ca757c
Justin McBride <jstnmcbrd@outlook.com>
2026-04-16 23:40:16 -0600

    Improve tests

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,11 +1,9 @@
-import { basename } from 'node:path';
-
-import { describe, it } from 'vitest';
+import { describe, test } from 'vitest';
 
 import delphi from '../src/index.js';
 
-describe(basename(import.meta.url), () => {
-	it('should succeed', async () => {
+describe('api', () => {
+	test('should succeed', async () => {
 		await delphi('Testing an API');
 	});
 });
